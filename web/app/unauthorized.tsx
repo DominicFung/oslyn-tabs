45e40f74b3cefa6b546c200c82e30f9bfc1a76b1
Dom Fung <fung_dominic@hotmail.com>
2023-12-31 15:08:54 -0500

    fix unauth page colouring

--- web/app/unauthorized.tsx
+++ web/app/unauthorized.tsx
@@ -27,7 +27,7 @@ export default function Unauth(p: UnauthProps) {
   }, [session, p.message])
 
   return <div className={`flex flex-row w-full h-[100vh] text-center align-middle justify-center`}>
-    <div className="text-white flex flex-col">
+    <div className="dark:text-white text-oslyn-900 flex flex-col">
       <div className="flex-1" />
       <div>
         <h1 className="inline-block mr-5 pr-6 text-2xl bold align-middle border-r border-gray-400">
@@ -39,8 +39,8 @@ export default function Unauth(p: UnauthProps) {
       </div>
       { status != "authenticated" && <div className="mt-4 flex w-full text-center align-middle justify-center">
         <button onClick={() => { setOpenLogin(true) }}
-          className="px-6 py-2 flex items-center p-2 text-gray-900 dark:text-gray-100 rounded-lg bg-oslyn-700 hover:bg-oslyn-800 focus:ring-4 focus:outline-none focus:ring-oslyn-300 font-medium text-sm text-center dark:bg-oslyn-600 dark:hover:bg-oslyn-700 dark:focus:ring-oslyn-800">
-        <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-oslyn-400 transition duration-75 dark:text-oslyn-200 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clipRule="evenodd"></path></svg>
+          className="px-6 py-2 flex items-center p-2 text-gray-100 rounded-lg bg-oslyn-700 hover:bg-oslyn-800 focus:ring-4 focus:outline-none focus:ring-oslyn-300 font-medium text-sm text-center dark:bg-oslyn-600 dark:hover:bg-oslyn-700 dark:focus:ring-oslyn-800">
+        <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 transition duration-75 text-oslyn-200 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clipRule="evenodd"></path></svg>
         <span className="flex-1 ml-3 whitespace-nowrap text-left">Sign In</span></button>
       </div> }
       <div className="flex-1" />
